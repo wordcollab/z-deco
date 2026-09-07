109e5732505ed00c409a6e21b1df9018ae5c8054
wordcollab <word42851@gmail.com>
2026-09-07 19:39:49 +0100

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// Keep this value equal to the GitHub repository name.
-export default defineConfig({
-  base: "/z-deco/",
-  plugins: [react()],
-});
